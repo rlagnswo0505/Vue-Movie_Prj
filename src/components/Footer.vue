ff0bf43608f8fec921bc1fd7701008e6bf79eca5
HoonJae <rlagnswo0505@naver.com>
2022-07-13 17:59:33 +0900

    create header footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <h4>Footer Area</h4>
+  </footer>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: '',
+    };
+  },
+};
+</script>
+
+<style scoped></style>
