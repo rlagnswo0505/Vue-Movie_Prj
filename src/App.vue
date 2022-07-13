7d8bfa55a7c64596b4527bea84ff79f0cf76061b
HoonJae <rlagnswo0505@naver.com>
2022-07-13 18:10:50 +0900

    기본 세팅 완료

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,16 @@
     <Footer></Footer>
   </div>
 </template>
-
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
-
-const routes = [];
+import Home from '../views/Home.vue';
+import BoxOfficeByDayView from '../views/BoxOfficeByDayView.vue';
+const routes = [
+  {
+    path: '/',
+    name: 'name',
+    component: Home,
+  },
+  {
+    path: '/boxOfficeByDay',
+    name: 'boxOfficeByDay',
+    component: BoxOfficeByDayView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
