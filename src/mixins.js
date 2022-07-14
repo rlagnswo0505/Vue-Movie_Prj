9ff86348e1df5ad72d2dd3162fd55f002ec6707a
HoonJae <rlagnswo0505@naver.com>
2022-07-14 10:14:24 +0900

    mixins.js 사용하기

---
+++ src/mixins.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export default {
+  methods: {
+    async $api(url, data) {
+      return (
+        await axios({
+          method: 'get',
+          url,
+          data,
+        }).catch((e) => {
+          console.log(e);
+        })
+      ).data;
+    },
+  },
+};
