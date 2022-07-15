3d3cfbfc4607137e3652475171a0890b329b1d0f
HoonJae <rlagnswo0505@naver.com>
2022-07-15 10:04:10 +0900

    기존 day, week 파일 삭제하기

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import BoxOfficeByDayView from '../views/BoxOfficeByDayView.vue';
-import BoxOfficeByWeek from '../views/BoxOfficeByWeek.vue';
+// import BoxOfficeByDayView from '../views/BoxOfficeByDayView.vue';
+// import BoxOfficeByWeek from '../views/BoxOfficeByWeek.vue';
 import BoxOffice from '../views/BoxOffice';
 const routes = [
   {
